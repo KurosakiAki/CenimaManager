d413a51d1cdeb4812834ffccdc69474f8de79fba
Hieu Huynh <40838918+KurosakiAki@users.noreply.github.com>
2023-03-16 13:47:15 +0700

    Add files via upload

---
+++ CinemaManagement/src/main/java/edu/fa/servlet/WelcomeServlet.java
@@ -0,0 +1,47 @@
+package edu.fa.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class WelcomeServlet
+ */
+@WebServlet("/")
+public class WelcomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public WelcomeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher("/index.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String name = request.getParameter("name");
+		String context = request.getContextPath();
+		response.sendRedirect(context + "/Hello?name=" + name);
+		
+	}
+
+}
---
+++ CinemaManagement/src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Welcome u</title>
+</head>
+<body>
+	<form action="WelcomeServlet" method="post">
+		<h4>Enter your name: </h4>
+		<input name="name" type="text" placeholder="Name">
+		<button type="submit">Next</button>
+	</form>
+</body>
+</html>
